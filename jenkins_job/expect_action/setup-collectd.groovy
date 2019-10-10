84e62d9c35efd687d53124e4ca0b1f0cef9fec3a
wsunccake <wsunccake@hotmail.com>
2019-10-10 22:36:51 +0800

    Update setup-collectd.groovy

--- jenkins_job/expect_action/setup-collectd.groovy
+++ jenkins_job/expect_action/setup-collectd.groovy
@@ -48,11 +48,13 @@ SN=`./get_sn.sh`
 echo "SN: $SN"
 
 # get passphrase
-echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}'"
-echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \$3}'"
-echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\$3}'"
-echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\\$3}'"
-echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\\\$3}'"
+echo "${SESAME2_URL}${SN}"
+
+#echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}'"
+#echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \$3}'"
+#echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\$3}'"
+#echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\\$3}'"
+#echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\\\$3}'"
 
 #echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}' | tr -d '"
 #echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}' | tr -d \'"
@@ -60,7 +62,7 @@ echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print \\\\$3}'"
 #echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}' | tr -d \\\'"
 #echo "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}' | tr -d \\\\'"
 
-#PASSPHRASE=`curl "${SESAME2_URL}${SN}" | awk '/Access Key/{print $3}' | tr -d \'`
+#PASSPHRASE=`curl ${SESAME2_URL}${SN} | awk '/Access Key/{print $3}' | tr -d \'`
 #export PASSPHRASE=$PASSPHRASE
 
 
