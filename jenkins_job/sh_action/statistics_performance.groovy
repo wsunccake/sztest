7b21769cac22ceb11b0b14b9db45c394979b51e6
wsunccake <wsunccake@hotmail.com>
2019-10-11 01:33:32 +0800

    Update statistics_performance.groovy

--- jenkins_job/sh_action/statistics_performance.groovy
+++ jenkins_job/sh_action/statistics_performance.groovy
@@ -24,16 +24,14 @@ pipeline {
             steps {
                 sh '''#!/bin/bash
 echo "data dir: $VAR_DIR/output/$VAR_DATA"
-#grep -A1 'Response code: 201' $VAR_DIR/output/$VAR_DATA/*.out
 
 echo "find $VAR_DIR/output/$VAR_DATA -name \\*.out -exec grep -A1 'Response code: 201' {} \\; \\
-| awk '/Response time:/ {print \\$3}' \\
+| awk '/Response time:/ {print \$3}' \\
 |  $API_PERF_DIR/util/statistics.awk"
 
 find $VAR_DIR/output/$VAR_DATA -name \\*.out -exec grep -A1 'Response code: 201' {} \\; \\
-| awk '/Response time:/ {print \\$3}' \\
+| awk '/Response time:/ {print \$3}' \\
 |  $API_PERF_DIR/util/statistics.awk
-
 '''
             }
         }
