ec3780aad623e1bccbe6738d2fe8102cb2836a0b
wsunccake <wsunccake@hotmail.com>
2019-10-13 12:47:55 +0800

    Add shutdown

---
+++ jenkins_job/sh_action/shutdown_sim_pc.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    parameters {
+        string(name: 'version', defaultValue: '1.0.0.0', description: '')
+        string(name: 'scenario', defaultValue: 'group0', description: '')
+
+        string(name: 'VAR_DIR', defaultValue: '/var/lib/jenkins/api_perf/var/${scenario}', description: '')
+    }
+
+    stages {
+        stage('Update Build Name') {
+            steps {
+                script {
+                    currentBuild.displayName = "${version} - ${scenario} - #${currentBuild.number}"
+                }
+
+            }
+        }
+
+        stage('Startup SimPC') {
+            steps {
+                sh '''#!/bin/bash
+if [ ! -f $VAR_DIR/input/sim/sim.inp ]; then
+  echo "no found $VAR_DIR/input/sim/sim.inp"
+  exit 1
+fi
+
+
+for sim_pc in `awk '{print \$1}' $VAR_DIR/input/sim/sim.inp`; do
+  echo "start: `date`"
+  time gcloud compute instances stop $sim_pc
+  echo "end: `date`"
+done
+'''
+            }
+        }
+    }
+}
