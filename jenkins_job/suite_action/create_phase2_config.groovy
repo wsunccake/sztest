55296abe3e02da0d84695d2a5fa87cf831e07a94
wsunccake <wsunccake@hotmail.com>
2020-02-12 02:57:23 +0000

    Update phase2 config

--- jenkins_job/suite_action/create_phase2_config.groovy
+++ jenkins_job/suite_action/create_phase2_config.groovy
@@ -5,8 +5,6 @@ node {
                         string(name: 'VAR_DIR', defaultValue: '/usr/share/nginx/html/api_perf/${version}/${scenario}', description: ''),
                         string(name: 'API_PERF_VER', defaultValue: 'v9_0', description: ''),
                         string(name: 'SZ_IP', defaultValue: '1.2.3.4', description: ''),
-                        string(name: 'AP_NUM', defaultValue: '1', description: ''),
-                        string(name: 'UE_NUM', defaultValue: '1', description: ''),
             ])
     ])
 
@@ -82,88 +80,13 @@ node {
         ]
     }
 
-//
-//    stage('Analyze WLAN') {
-//        build job: 'statistics_performance', parameters: [string(name: 'version', value: "${version}"),
-//                                                          string(name: 'scenario', value: "${scenario}"),
-//                                                          string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                          string(name: 'VAR_DATA', value: "wlans"),
-//        ]
-//    }
-//
-//    stage('Create DPSK ') {
-//        build job: 'create_dpsk_batch', parameters: [string(name: 'version', value: "${version}"),
-//                                                     string(name: 'scenario', value: "${scenario}"),
-//                                                     string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                     string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-//                                                     string(name: 'SZ_IP', value: "${SZ_IP}"),
-//                                                     string(name: 'DPSK_AMOUNT', value: "${DPSK_AMOUNT}"),
-//        ]
-//    }
-//
-//    stage('Analyze DPSK') {
-//        build job: 'statistics_performance', parameters: [string(name: 'version', value: "${version}"),
-//                                                          string(name: 'scenario', value: "${scenario}"),
-//                                                          string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                          string(name: 'VAR_DATA', value: "wlans/dpsk"),
-//        ]
-//    }
-//
-//    stage('Create WLAN Group') {
-//        build job: 'create_wlan_group', parameters: [string(name: 'version', value: "${version}"),
-//                                                     string(name: 'scenario', value: "${scenario}"),
-//                                                     string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                     string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-//                                                     string(name: 'SZ_IP', value: "${SZ_IP}"),
-//        ]
-//    }
-//
-//    stage('Analyze WLAN Group') {
-//        build job: 'statistics_performance', parameters: [string(name: 'version', value: "${version}"),
-//                                                          string(name: 'scenario', value: "${scenario}"),
-//                                                          string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                          string(name: 'VAR_DATA', value: "wlan_groups"),
-//        ]
-//    }
-//
-//    stage('Pre-Provision AP') {
-//        build job: 'create_ap', parameters: [string(name: 'version', value: "${version}"),
-//                                             string(name: 'scenario', value: "${scenario}"),
-//                                             string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                             string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-//                                             string(name: 'SZ_IP', value: "${SZ_IP}"),
-//        ]
-//    }
-//
-//    stage('Analyze AP') {
-//        build job: 'statistics_performance', parameters: [string(name: 'version', value: "${version}"),
-//                                                          string(name: 'scenario', value: "${scenario}"),
-//                                                          string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                          string(name: 'VAR_DATA', value: "aps"),
-//        ]
-//    }
-//
-//    stage('Create AP Group') {
-//        build job: 'create_ap_group', parameters: [string(name: 'version', value: "${version}"),
-//                                                   string(name: 'scenario', value: "${scenario}"),
-//                                                   string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                   string(name: 'SZ_IP', value: "${SZ_IP}"),
-//        ]
-//    }
-//
-//    stage('Analyze AP Group') {
-//        build job: 'statistics_performance', parameters: [string(name: 'version', value: "${version}"),
-//                                                          string(name: 'scenario', value: "${scenario}"),
-//                                                          string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//                                                          string(name: 'VAR_DATA', value: "ap_groups"),
-//        ]
-//    }
-//
-//    stage('Arrange Data') {
-//        build job: 'pickup_data', parameters: [string(name: 'version', value: "${version}"),
-//                                               string(name: 'scenario', value: "${scenario}"),
-//                                               string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-//        ]
-//    }
+    stage('Pre-Provision AP') {
+        build job: 'create_ap', parameters: [string(name: 'version', value: "${version}"),
+                                             string(name: 'scenario', value: "${scenario}"),
+                                             string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                                             string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                                             string(name: 'SZ_IP', value: "${SZ_IP}"),
+        ]
+    }
 
 }
