bc040a933be978bec3616c51ec6c4fb533f04ebe
wsunccake <wsunccake@hotmail.com>
2020-03-13 03:33:00 +0000

    support phase2

--- jenkins_job/sh_action/create_csv.groovy
+++ jenkins_job/sh_action/create_csv.groovy
@@ -25,17 +25,25 @@ pipeline {
 set -e
 
 
-VAR_FILES=("domains" "zones" "wlans" "wlan_groups" "aps" "ap_groups")
+PHASE1_FILES=(domains zones wlans wlan_groups aps ap_groups)
+PHASE2_FILES=(domains zones wlans aps hotspot \\
+             non_proxy_acct non_proxy_auth guest_access \\
+             dhcppool etherport client_isolation \\
+             ap_rule subscription_package)
+VAR_FILES=(${PHASE1_FILES[*]} ${PHASE2_FILES[*]})
+VAR_FILES=(`echo "${VAR_FILES[*]}" | tr ' ' '\\n' | sort -u | tr '\\n' ' '`)
 
 mkdir -p $DATA_DIR/create
 
 echo "create api data:"
 for var_file in ${VAR_FILES[*]}; do
-  echo "data file: $VAR_DIR/output/response_time/$var_file -> ${var_file}.csv"
-  echo "$API_PERF_DIR/util/statistics.awk $VAR_DIR/output/response_time/${var_file}.log | $API_PERF_DIR/util/post_created_data.awk"
-  tmp_data=`$API_PERF_DIR/util/statistics.awk $VAR_DIR/output/response_time/${var_file}.log | $API_PERF_DIR/util/post_created_data.awk`
-  echo "$version,$tmp_data" >> $DATA_DIR/create/${var_file}.csv
-  echo "$version,$tmp_data >> $DATA_DIR/create/${var_file}.csv"
+  if [[ -f $VAR_DIR/output/response_time/${var_file}.log ]]; then
+    echo "data file: $VAR_DIR/output/response_time/$var_file -> ${var_file}.csv"
+    echo "$API_PERF_DIR/util/statistics.awk $VAR_DIR/output/response_time/${var_file}.log | $API_PERF_DIR/util/post_created_data.awk"
+    tmp_data=`$API_PERF_DIR/util/statistics.awk $VAR_DIR/output/response_time/${var_file}.log | $API_PERF_DIR/util/post_created_data.awk`
+    echo "$version,$tmp_data" >> $DATA_DIR/create/${var_file}.csv
+    echo "$version,$tmp_data >> $DATA_DIR/create/${var_file}.csv"
+  fi
 done
 '''
             }
@@ -47,20 +55,28 @@ done
 set -e
 
 
-VAR_FILES=("01_query_wlan" "02_query_dpsk" "03_query_ap" "04_rkszones_zoneId_wlan" "05_rkszones" \\
-"06_rkszones_id_apgroups" "07_rkszones_id_wlangroups" "09_domain_id_subdomain" "10_domain" "11_aps")
+PHASE1_FILES=(query_wlan query_dpsk query_ap rkszones_zoneId_wlan rkszones \\
+              rkszones_id_apgroups rkszones_id_wlangroups domain_id_subdomain \\
+              domain aps)
+PHASE2_FILES=(query_criteria_client_isolation query_criteria_etherport query_criteria_guest_access \\
+              query_criteria_hotspot query_criteria_non_proxy_acct query_criteria_non_proxy_auth \\
+              query_criteria_subscriber_package query_criteria_wlan) 
+VAR_FILES=(${PHASE1_FILES[*]} ${PHASE2_FILES[*]})
+VAR_FILES=(`echo "${VAR_FILES[*]}" | tr ' ' '\\n' | sort -u | tr '\\n' ' '`)
 
 mkdir -p $DATA_DIR/query
 
 echo "query api data:"
 for var_file in ${VAR_FILES[*]}; do
-  echo "data dir: $VAR_DIR/output/api_performance/$var_file -> ${var_file#*_}.csv"
-  echo "grep -Pzo 'Running teardowns[\\s\\S]+Percentage' $VAR_DIR/output/api_performance/${var_file}.log \\
-  | grep -Pzo '\\-[\\s\\S]+[\\-]' | grep -v '-' | grep -v 'session' | awk '{printf "%d,%d,%d,%d\\n", \\$5, \\$6, \\$7, \\$9}\'"
-  tmp_data=`grep -Pzo 'Running teardowns[\\s\\S]+Percentage' $VAR_DIR/output/api_performance/${var_file}.log \\
-  | grep -Pzo '\\-[\\s\\S]+[\\-]' | grep -v '-' | grep -v 'session' | awk '{printf "%d,%d,%d,%d\\n", \$5, \$6, \$7, \$9}'`
-  echo "$version,$tmp_data" >> $DATA_DIR/query/${var_file#*_}.csv
-  echo "$version,$tmp_data >> $DATA_DIR/query/${var_file#*_}.csv"
+  if [[ -f $VAR_DIR/output/api_performance/${var_file}.log ]]; then
+    echo "data dir: $VAR_DIR/output/api_performance/$var_file -> ${var_file#*_}.csv"
+    echo "grep -Pzo 'Running teardowns[\\s\\S]+Percentage' $VAR_DIR/output/api_performance/${var_file}.log \\
+    | grep -Pzo '\\-[\\s\\S]+[\\-]' | grep -v '-' | grep -v 'session' | awk '{printf "%d,%d,%d,%d\\n", \\$5, \\$6, \\$7, \\$9}\'"
+    tmp_data=`grep -Pzo 'Running teardowns[\\s\\S]+Percentage' $VAR_DIR/output/api_performance/${var_file}.log \\
+    | grep -Pzo '\\-[\\s\\S]+[\\-]' | grep -v '-' | grep -v 'session' | awk '{printf "%d,%d,%d,%d\\n", \$5, \$6, \$7, \$9}'`
+    echo "$version,$tmp_data" >> $DATA_DIR/query/${var_file#*_}.csv
+    echo "$version,$tmp_data >> $DATA_DIR/query/${var_file#*_}.csv"
+  fi
 done
 '''
             }
