5046ae54defa2c4b06b34446190f207a7249e619
wsunccake <wsunccake@hotmail.com>
2020-06-26 09:20:52 +0800

    update sim_pc_ssh_status

--- jenkins_job/sh_action/sim_pc_ssh_status.groovy
+++ jenkins_job/sh_action/sim_pc_ssh_status.groovy
@@ -21,8 +21,22 @@ pipeline {
         stage('Sim PC Connection Status') {
             steps {
                 sh '''#!/bin/bash
-ssh_timeout=3
-awk '{print \$2}' $VAR_DIR/input/sim/sim.inp | xargs -P ${NPROC} -i sh -c "ssh -vvv -o ConnectTimeout=$ssh_timeout {} date >& $VAR_DIR/input/sim/{}.log && echo 'pass: {}' || echo 'fail: {}'"
+ap_ue_state() {
+  local h=$1
+  local ssh_timeout=3
+  local is_ssh_connect=`ssh -vvv -o ConnectTimeout=$ssh_timeout $h date >& $VAR_DIR/input/sim/$h.log && echo true || echo false`
+
+  if [ "$is_ssh_connect" == "true" ]; then
+    ssh $h 'sudo su - -c "cd /opt/madSZ/BUILD/scripts && ./madutil -v"' 2> /dev/null 1>> $VAR_DIR/input/sim/$h.log
+    ssh $h 'sudo su - -c "ls /dev/shm/sim/ue | xargs -i /opt/madSZ/BUILD/bin/dumpTool -s {}"' 2> /dev/null 1>> $VAR_DIR/input/sim/$h.log
+    echo "ssh connectio pass: $h"
+  else
+    echo "ssh connectio fail: $h"
+  fi
+}
+export -f ap_ue_state
+
+awk '{print \$2}' $VAR_DIR/input/sim/sim.inp | xargs -P ${NPROC} -i sh -c 'ap_ue_state {}'
 '''
             }
         }
