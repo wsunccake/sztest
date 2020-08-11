917ee11e0d8703733e8a95ff0cebce2095ab17ea
wsunccake <wsunccake@hotmail.com>
2020-08-11 06:01:32 +0000

    update partner phase1

--- jenkins_job/scenario/partner.groovy
+++ jenkins_job/scenario/partner.groovy
@@ -4,7 +4,7 @@ node {
     properties([
             parameters([
                     // common
-                    string(name: 'SZ_VERSION', defaultValue: '5.2.1.0'),
+                    string(name: 'SZ_VERSION', defaultValue: '5.2.1.1'),
                     string(name: 'SCENARIO', defaultValue: 'partner'),
                     string(name: 'VAR_DIR', defaultValue: '/usr/share/nginx/html/api_perf/${SZ_VERSION}/${SCENARIO}', description: ''),
 
@@ -16,7 +16,7 @@ node {
                     string(name: 'SZ_NUM', defaultValue: '1', description: ''),
 
                     // end2end_join_associate
-                    string(name: 'AP_VERSION', defaultValue: '5.2.1.0'),
+                    string(name: 'AP_VERSION', defaultValue: '5.2.1.1'),
                     string(name: 'AP_MODEL', defaultValue: 'R710', description: ''),
                     string(name: 'AP_NUM', defaultValue: '9999', description: ''),
                     string(name: 'UE_NUM', defaultValue: '90000', description: ''),
@@ -29,11 +29,11 @@ node {
                     string(name: 'is_clean_env', defaultValue: 'true', description: ''),
 
                     string(name: 'API_PERF_VER', defaultValue: 'v9_1', description: ''),
-                    string(name: 'DATA_DIR', defaultValue: '/usr/share/nginx/html/api_perf/5.2/report/${SCENARIO}', description: ''),
+                    string(name: 'DATA_DIR', defaultValue: '/usr/share/nginx/html/api_perf/5.2.1.1/report/${SCENARIO}', description: ''),
 
-
-                    string(name: 'NUM_CLIENT', defaultValue: '2', description: ''),
-                    string(name: 'HATCH_RATE', defaultValue: '1', description: ''),
+                    // query_api
+                    string(name: 'NUM_CLIENT', defaultValue: '10', description: ''),
+                    string(name: 'HATCH_RATE', defaultValue: '2', description: ''),
                     string(name: 'RUN_TIME', defaultValue: '20m1s', description: ''),
             ])
     ])
@@ -124,16 +124,16 @@ node {
 
     stage('Test Query API') {
         if (params.is_skip_query == "false") {
-            build job: 'query_api', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                 string(name: 'scenario', value: "${SCENARIO}"),
-                                                 string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                 string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                 string(name: 'TASK_DIR', value: 'phase1'),
-                                                 string(name: 'SZ_IP', value: "${szIP}"),
-                                                 string(name: 'NUM_CLIENT', value: "${NUM_CLIENT}"),
-                                                 string(name: 'HATCH_RATE', value: "${HATCH_RATE}"),
-                                                 string(name: 'RUN_TIME', value: "${RUN_TIME}"),
-
+            build job: 'query_api', parameters: [
+                    string(name: 'version', value: "${SZ_VERSION}"),
+                    string(name: 'scenario', value: "${SCENARIO}"),
+                    string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                    string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                    string(name: 'TASK_DIR', value: 'phase1'),
+                    string(name: 'SZ_IP', value: "${szIP}"),
+                    string(name: 'NUM_CLIENT', value: "${NUM_CLIENT}"),
+                    string(name: 'HATCH_RATE', value: "${HATCH_RATE}"),
+                    string(name: 'RUN_TIME', value: "${RUN_TIME}"),
             ]
         } else {
             echo "Skip to Test Query API"
@@ -142,12 +142,12 @@ node {
 
     stage('Create CSV') {
         if (params.is_skip_csv == "false") {
-            build job: 'create_csv', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                  string(name: 'scenario', value: "${SCENARIO}"),
-                                                  string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                  string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                  string(name: 'DATA_DIR', value: "${DATA_DIR}"),
-
+            build job: 'create_csv', parameters: [
+                    string(name: 'version', value: "${SZ_VERSION}"),
+                    string(name: 'scenario', value: "${SCENARIO}"),
+                    string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                    string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                    string(name: 'DATA_DIR', value: "${DATA_DIR}"),
             ]
         } else {
             echo "Skip to Create CSV"
--- jenkins_job/scenario/phase1.groovy
+++ jenkins_job/scenario/phase1.groovy
@@ -3,27 +3,40 @@ def szIP
 node {
     properties([
             parameters([
-                    string(name: 'SZ_VERSION', defaultValue: '5.2.1.0'),
-                    string(name: 'SCENARIO', defaultValue: 'phase1'),
-                    string(name: 'AP_VERSION', defaultValue: '5.2.1.0'),
-                    string(name: 'SRC_DIR', defaultValue: '/var/lib/jenkins/api_perf/var/${SCENARIO}', description: ''),
+                    // common
+                    string(name: 'SZ_VERSION', defaultValue: '5.2.1.1'),
+                    string(name: 'SCENARIO', defaultValue: 'partner'),
                     string(name: 'VAR_DIR', defaultValue: '/usr/share/nginx/html/api_perf/${SZ_VERSION}/${SCENARIO}', description: ''),
 
-                    string(name: 'API_PERF_VER', defaultValue: 'v9_1', description: ''),
+                    // prepare_copy_var_dir
+                    string(name: 'SRC_DIR', defaultValue: '/var/lib/jenkins/sztest/conf/${SCENARIO}', description: ''),
 
+                    // suite_sz_setup
+                    string(name: 'GCE_IMAGE', defaultValue: 'vscg-cloud-${SZ_VERSION}', description: ''),
                     string(name: 'SZ_NUM', defaultValue: '1', description: ''),
-                    string(name: 'AP_NUM', defaultValue: '10000', description: ': group1: 6000, group2: 2000, group3: 2000'),
-                    string(name: 'UE_NUM', defaultValue: '100000', description: ' group1: 48000, group2: 48000, group3: 4000'),
-                    string(name: 'DPSK_AMOUNT', defaultValue: "10", description: ''),
+
+                    // end2end_join_associate
+                    string(name: 'AP_VERSION', defaultValue: '5.2.1.1'),
+                    string(name: 'AP_MODEL', defaultValue: 'R710', description: ''),
+                    string(name: 'AP_NUM', defaultValue: '9999', description: ''),
+                    string(name: 'UE_NUM', defaultValue: '90000', description: ''),
                     string(name: 'MADSZ_TGZ', defaultValue: 'madSZ-v5.2.1-14-u1804.tar.xz  ', description: ''),
 
-                    string(name: 'DATA_DIR', defaultValue: '/usr/share/nginx/html/api_perf/5.2/report/${SCENARIO}', description: ''),
-
-                    string(name: 'is_skip_join', defaultValue: 'false', description: ''),
+                    // stage control
+                    string(name: 'is_skip_end2end', defaultValue: 'false', description: ''),
                     string(name: 'is_skip_query', defaultValue: 'false', description: ''),
                     string(name: 'is_skip_csv', defaultValue: 'false', description: ''),
                     string(name: 'is_clean_env', defaultValue: 'true', description: ''),
 
+                    string(name: 'API_PERF_VER', defaultValue: 'v9_1', description: ''),
+
+                    // suite_phase1_config
+                    string(name: 'DPSK_AMOUNT', defaultValue: "10", description: ''),
+
+                    // create_csv
+                    string(name: 'DATA_DIR', defaultValue: '/usr/share/nginx/html/api_perf/5.2.1.1/report/${SCENARIO}', description: ''),
+
+                    // query_api
                     string(name: 'NUM_CLIENT', defaultValue: '2', description: ''),
                     string(name: 'HATCH_RATE', defaultValue: '1', description: ''),
                     string(name: 'RUN_TIME', defaultValue: '20m1s', description: ''),
@@ -34,13 +47,13 @@ node {
 
     stage('Prepare Var Dir') {
         build job: 'prepare_copy_var_dir',
-              parameters: [
-                      string(name: 'SZ_VERSION', value: "${SZ_VERSION}"),
-                      string(name: 'SCENARIO', value: "${SCENARIO}"),
-                      string(name: 'SRC_DIR', value: "${SRC_DIR}"),
-                      string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-              ],
-              propagate: false
+                parameters: [
+                        string(name: 'SZ_VERSION', value: "${SZ_VERSION}"),
+                        string(name: 'SCENARIO', value: "${SCENARIO}"),
+                        string(name: 'SRC_DIR', value: "${SRC_DIR}"),
+                        string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                ],
+                propagate: false
     }
 
     stage('Setup SZ') {
@@ -66,29 +79,32 @@ node {
 
     stage('Create Config') {
         build job: 'suite_phase1_config',
-              parameters: [
-                      string(name: 'SZ_VERSION', value: "${SZ_VERSION}"),
-                      string(name: 'SCENARIO', value: "${SCENARIO}"),
-                      string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                      string(name: 'SZ_IP', value: "${szIP}"),
-                      string(name: 'DPSK_AMOUNT', value: "${DPSK_AMOUNT}"),
-              ]
+                parameters: [
+                        string(name: 'SZ_VERSION', value: "${SZ_VERSION}"),
+                        string(name: 'SCENARIO', value: "${SCENARIO}"),
+                        string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                        string(name: 'SZ_IP', value: "${szIP}"),
+                        string(name: 'DPSK_AMOUNT', value: "${DPSK_AMOUNT}"),
+                ]
     }
 
 
-    stage('Join AP and UE') {
-        if (params.is_skip_join == "false") {
-            build job: 'join_ap_ue', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                  string(name: 'scenario', value: "${SCENARIO}"),
-                                                  string(name: 'ap_version', value: "${ap_version}"),
-                                                  string(name: 'SRC_DIR', value: "${SRC_DIR}"),
-                                                  string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                  string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                  string(name: 'SZ_IP', value: "${szIP}"),
-                                                  string(name: 'AP_NUM', value: "${AP_NUM}"),
-                                                  string(name: 'UE_NUM', value: "${UE_NUM}"),
-                                                  string(name: 'MADSZ_TGZ', value: "${MADSZ_TGZ}"),
-            ]
+    stage('Join AP and Associate UE') {
+        if (params.is_skip_end2end == "false") {
+            build job: 'suite_end2end_join_and_associate',
+                    parameters: [
+                            // common
+                            string(name: 'SZ_VERSION', value: "${SZ_VERSION}"),
+                            string(name: 'SCENARIO', value: "${SCENARIO}"),
+                            string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                            string(name: 'SZ_IP', value: "${szIP}"),
+
+                            string(name: 'AP_VERSION', value: "${AP_VERSION}"),
+                            string(name: 'AP_MODEL', value: "${AP_MODEL}"),
+                            string(name: 'AP_NUM', value: "${AP_NUM}"),
+                            string(name: 'UE_NUM', value: "${UE_NUM}"),
+                            string(name: 'MADSZ_TGZ', value: "${MADSZ_TGZ}"),
+                    ]
         } else {
             echo "Skip to Join AP and UE"
         }
@@ -96,16 +112,16 @@ node {
 
     stage('Test Query API') {
         if (params.is_skip_query == "false") {
-            build job: 'query_api', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                 string(name: 'scenario', value: "${SCENARIO}"),
-                                                 string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                 string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                 string(name: 'TASK_DIR', value: 'phase1'),
-                                                 string(name: 'SZ_IP', value: "${szIP}"),
-                                                 string(name: 'NUM_CLIENT', value: "${NUM_CLIENT}"),
-                                                 string(name: 'HATCH_RATE', value: "${HATCH_RATE}"),
-                                                 string(name: 'RUN_TIME', value: "${RUN_TIME}"),
-
+            build job: 'query_api', parameters: [
+                    string(name: 'version', value: "${SZ_VERSION}"),
+                    string(name: 'scenario', value: "${SCENARIO}"),
+                    string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                    string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                    string(name: 'TASK_DIR', value: 'phase1'),
+                    string(name: 'SZ_IP', value: "${szIP}"),
+                    string(name: 'NUM_CLIENT', value: "${NUM_CLIENT}"),
+                    string(name: 'HATCH_RATE', value: "${HATCH_RATE}"),
+                    string(name: 'RUN_TIME', value: "${RUN_TIME}"),
             ]
         } else {
             echo "Skip to Test Query API"
@@ -114,11 +130,12 @@ node {
 
     stage('Create CSV') {
         if (params.is_skip_csv == "false") {
-            build job: 'create_csv', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                  string(name: 'scenario', value: "${SCENARIO}"),
-                                                  string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                  string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                  string(name: 'DATA_DIR', value: "${DATA_DIR}"),
+            build job: 'create_csv', parameters: [
+                    string(name: 'version', value: "${SZ_VERSION}"),
+                    string(name: 'scenario', value: "${SCENARIO}"),
+                    string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                    string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                    string(name: 'DATA_DIR', value: "${DATA_DIR}"),
 
             ]
         } else {
@@ -128,11 +145,12 @@ node {
 
     stage('Clean Env') {
         if (params.is_clean_env == "true") {
-            build job: 'clean_env', parameters: [string(name: 'version', value: "${SZ_VERSION}"),
-                                                 string(name: 'scenario', value: "${SCENARIO}"),
-                                                 string(name: 'VAR_DIR', value: "${VAR_DIR}"),
-                                                 string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
-                                                 string(name: 'SZ_IP', value: "${szIP}"),
+            build job: 'clean_env', parameters: [
+                    string(name: 'version', value: "${SZ_VERSION}"),
+                    string(name: 'scenario', value: "${SCENARIO}"),
+                    string(name: 'VAR_DIR', value: "${VAR_DIR}"),
+                    string(name: 'API_PERF_VER', value: "${API_PERF_VER}"),
+                    string(name: 'SZ_IP', value: "${szIP}"),
             ]
         } else {
             echo "Skip to Clean Env"
