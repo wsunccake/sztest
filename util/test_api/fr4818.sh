b50c4a85576e74138829f4e9a560ccc4a956ec74
kam <kam@a1.arrs.arrisi.com>
2020-12-04 05:12:59 +0800

    add fr4818 api

---
+++ util/test_api/fr4818.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+create_zone() {
+  local name=$1
+  local domain_id=$2
+  domain_id=${domain_id:="${DEFAULT_DOMAIN_UUID}"}
+  local data="{
+    \"domainId\": \"${domain_id}\",
+    \"name\": \"${name}\",
+    \"login\": {
+        \"apLoginName\": \"${AP_USERNAME}\",
+        \"apLoginPassword\": \"${AP_PASSWORD}\"
+    }
+}"
+
+  declare -A api_data=(['url']=${PUBAPI_BASE_URL}/rkszones ['data']=$data)
+  pubapi_post "$(declare -p api_data)"
+}
+
+create_lbs() {
+  local name=$1
+  local domain_id=$2
+  domain_id=${domain_id:-"${DEFAULT_DOMAIN_UUID}"}
+  local data="{
+    \"venue\": \"${name}\",
+    \"domainId\": \"${domain_id}\",
+    \"url\": \"1.2.3.4\",
+    \"port\": 8883,
+    \"password\": \"admin!234\"
+}"
+
+  declare -A api_data=(['url']=${PUBAPI_BASE_URL}/profiles/lbs ['data']=$data)
+  pubapi_post "$(declare -p api_data)"
+}
+
+
+create_ap_group() {
+  local name=$1
+  local zone_id=$2
+  local data="{
+    \"name\": \"${name}\"
+}"
+  declare -A api_data=(['url']=${PUBAPI_BASE_URL}/rkszones/${zone_id}/apgroups ['data']=$data)
+  pubapi_post "$(declare -p api_data)"
+}
+
+###
+### export function
+###
+
+export -f create_zone
+export -f create_lbs
+export -f create_ap_group
